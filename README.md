885a908879e928fd6ecb87b5e600d232ab9eac37
Oleg Lavrovsky <oleg@utou.ch>
2016-12-15 09:30:08 +0100

    Added jsonp option

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ To build `ckan-embed.js` and view the test examples, you must have [npm](https:/
 1. Run `npm install` in the ckan-embed folder to install dependencies.
 2. Run `npm run build`. This will invoke [browserify](http://browserify.org/) to bundle the source files, and then [uglify-js](http://lisperator.net/uglifyjs/) to create the minified version.
 3. Run a local webserver (e.g., `python -m SimpleHTTPServer 8000`) in the root folder and then point your web browser at the test directory (e.g., `http://localhost:8000/test/`).
+
+## Usage notes
+
+For information on the JavaScript CKAN client see [ckan.js](https://github.com/okfn/ckan.js), for details of API usage see [docs.ckan.org](http://docs.ckan.org/en/latest/api/).
+
+A web server like [NGINX can be used](https://www.nginx.com/resources/admin-guide/reverse-proxy/) to proxy requests and avoid the use of JSONP.
--- src/embed.js
+++ src/embed.js
@@ -4,6 +4,56 @@ var lodash = require('lodash'),
 
 var config = {};
 
+/* Support function to publish data to page */
+function generateView(ckandata) {
+
+  var datasets = ckandata.result.results;
+  console.debug(datasets);
+
+  // Generate HTML of the widget
+  var template = _.template(
+    '<div class="ckan-dataset">' +
+    '<a href="<%= ds.url %>">' +
+    '<h5><%= ds.title %></h5>' +
+    '</a>' +
+    '<p><%= ds.description %></p>' +
+    '<b><%= ds.groupname %></b><br>' +
+    '<small><%= ds.formats %></small>' +
+    //'<small><%= ds.modified %></small>' +
+    '</div>'
+  );
+
+  // Helper functions to massage the results
+  var fragments = [];
+  var getLangDefault = function(n) {
+    return n.fr || n.de || n.it || n.en;
+  };
+  var getDatasetFormats = function(res) {
+    return _.uniq(_.map(res,
+      function(r) { return r.format }))
+      .join(' ');
+    };
+
+    // Pass the dataset results to the template
+    for (var i in datasets) {
+      var dso = datasets[i];
+      var ds = {
+        url:           url + 'en/dataset/' + dso.name,
+        title:         getLangDefault(dso.display_name),
+        groupname:     (dso.groups.length == 0) ? '' :
+        getLangDefault(dso.groups[0].display_name),
+        description:   getLangDefault(dso.description),
+        formats:       getDatasetFormats(dso.resources),
+        modified:      dso.metadata_modified
+      };
+      fragments.push(template({ ds: ds }));
+    }
+
+    // Insert into the container on the page
+    div.html(fragments.join(''));
+
+  }
+
 // Embed a CKAN result in a web page.
 // el: DOM element in which to place component (DOM node or CSS selector)
 // url: Source portal (URL string)
@@ -13,7 +63,7 @@ function embed(el, url, query, callback) {
   var cb = callback || function(){},
       client, config, action;
 
-      //try {
+  try {
     // Parse CKAN query
     if (_.isString(query)) {
       config = { q: query };
@@ -28,6 +78,9 @@ function embed(el, url, query, callback) {
     } else {
       action = config.action;
     }
+    if (_.isUndefined(config.jsonp)) {
+      config.jsonp = true;
+    }
 
     // ensure container div has class
     var div = $(el)
@@ -37,71 +90,34 @@ function embed(el, url, query, callback) {
     // create a client
     var client = new CKAN.Client(url);
 
-    // revise ckan.js action routine with jsonp support
-    if (action.indexOf('dataset_' === 0)) {
-      action = action.replace('dataset_', 'package_');
-    }
-    var options = {
-      url: client.endpoint + '/3/action/' + action,
-      data: config, dataType: "jsonp",
-      type: 'POST', cache: true
-    };
-
-    $.ajax(options)
-    .fail(function(err) {
-      if (err != null) { cb(err); return; }
-    })
-    .done(function(result) {
-
-      var datasets = result.result.results;
-      console.log(datasets);
-
-      // Generate HTML of the widget
-      var template = _.template(
-        '<div class="ckan-dataset">' +
-        '<a href="<%= ds.url %>">' +
-        '<h5><%= ds.title %></h5>' +
-        '</a>' +
-        '<p><%= ds.description %></p>' +
-        '<b><%= ds.groupname %></b><br>' +
-        '<small><%= ds.formats %></small>' +
-        //'<small><%= ds.modified %></small>' +
-        '</div>'
-      );
-
-      // Helper functions to massage the results
-      var fragments = [];
-      var getLangDefault = function(n) {
-        return n.fr || n.de || n.it || n.en;
-      };
-      var getDatasetFormats = function(res) {
-        return _.uniq(_.map(res,
-          function(r) { return r.format }))
-          .join(' ');
-      };
-
-      // Pass the dataset results to the template
-      for (var i in datasets) {
-        var dso = datasets[i];
-        var ds = {
-          url:           url + 'en/dataset/' + dso.name,
-          title:         getLangDefault(dso.display_name),
-          groupname:     (dso.groups.length == 0) ? '' :
-          getLangDefault(dso.groups[0].display_name),
-          description:   getLangDefault(dso.description),
-          formats:       getDatasetFormats(dso.resources),
-          modified:      dso.metadata_modified
-        };
-        fragments.push(template({ ds: ds }));
+    // extend ckan.js action routine with jsonp support
+    if (config.jsonp) {
+      if (action.indexOf('dataset_' === 0)) {
+        action = action.replace('dataset_', 'package_');
       }
+      var options = {
+        url: client.endpoint + '/3/action/' + action,
+        data: config, dataType: "jsonp",
+        type: 'POST', cache: true
+      };
 
-      // Insert into the container on the page
-      div.html(fragments.join(''));
+      $.ajax(options)
+      .fail(function(err) {
+        if (err != null) { cb(err); return; }
+      })
+      .done(generateView);
 
-    });
+    } else {
+      // recommended default usage of ckan.js, e.g. through CORS or proxy
+      client.action(action, config, function(err, result) {
+        if (err != null) { cb(err); return; }
+
+        generateView(result);
+      });
+    }
 
   cb(null, {client: client, config: config});
-  //} catch (err) { cb(err); }
+  } catch (err) { cb(err); }
 
 }
 
